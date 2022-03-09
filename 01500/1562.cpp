cae961182bc411fa35f188433b741afbe7c7b8bb
Phildy <86285421+pianoop@users.noreply.github.com>
2022-03-09 12:25:52 +0900

    baekjoon algorithm
    Dynamic programing
    Bitmasking

---
+++ 01500/1562.cpp
@@ -0,0 +1,101 @@
+//계단 수
+//-------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+#define ll long long
+typedef std::vector<ll> vl;
+typedef std::vector<vl> vvl;
+
+class Solve
+{
+public:
+	Solve(int n) :N(n)
+	{
+		DP.resize(10, vvl(N, vl(1024,-1)));   //[10][N][1024]
+		subDP.resize(N - 9, vl(10));         //[N-9][10]
+	
+		FillsubDP();
+
+		for (int i = 1; i < 10; i++)
+		{
+			ans += FillDP(i,N - 1,0);
+			ans %= div;
+		}
+	}
+
+	ll FillDP(int now, int depth, int visitbit)
+	{
+		visitbit |= (1 << now);
+		if (DP[now][depth][visitbit] != -1)
+		{
+			return DP[now][depth][visitbit];
+		}
+		if (visitbit == 1023)
+		{
+			DP[now][depth][visitbit] = subDP[depth][now];
+			return DP[now][depth][visitbit];
+		}
+		if (depth == 0)
+			return 0;
+
+		DP[now][depth][visitbit] = 0;
+		if (now != 9)
+			DP[now][depth][visitbit] += FillDP(now + 1, depth - 1, visitbit);
+		if (now != 0)
+			DP[now][depth][visitbit] += FillDP(now - 1, depth - 1, visitbit);
+
+		DP[now][depth][visitbit] %= div;
+		return DP[now][depth][visitbit];
+	}
+
+	void FillsubDP()
+	{
+		std::fill(subDP[0].begin(), subDP[0].end(), 1);
+
+		for (int i = 1; i < int(subDP.size()); i++)
+		{
+			subDP[i][0] = subDP[i - 1][1];
+			for (int j = 1; j < 9; j++)
+			{
+				subDP[i][j] = (subDP[i - 1][j - 1] + subDP[i - 1][j + 1]) % div;
+			}
+			subDP[i][9] = subDP[i - 1][8];
+		}
+	}
+
+	void Printans()
+	{
+		printf("%lld", ans);
+	}
+
+	ll Putans()
+	{
+		return ans;
+	}
+
+private:
+	const int div = 1'000'000'000;
+	int N;
+	ll ans = 0;
+	std::vector<vvl> DP;
+	vvl subDP;
+};
+
+int main()
+{
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+
+	int N;
+	std::cin >> N;
+	if (N > 10)
+	{
+		Solve sol(N);
+		sol.Printans();
+	}
+	else if (N == 10)
+		printf("1");
+	else
+		printf("0");
+
+	return 0;
+}
