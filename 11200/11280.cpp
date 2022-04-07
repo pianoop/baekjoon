e92545c6f837bcd0cc0f4d6d5ededd9f26f2a841
Phildy <86285421+pianoop@users.noreply.github.com>
2022-04-07 11:03:28 +0900

    baekjoon algorithm
    Graph
    SCC
    2-sat

---
+++ 11200/11280.cpp
@@ -0,0 +1,166 @@
+//2-SAT - 3
+//-------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+typedef std::vector<int> vi;
+typedef std::vector<vi> vvi;
+
+class SCC
+{
+public:
+	SCC()
+	{
+		input();
+		calc();
+	}
+
+	~SCC()
+	{
+		delete[] mbCheck;
+		delete[] mbSccCheck;
+	}
+
+	void PrintAns()
+	{
+		if (mbAns)
+			std::cout << 1;
+		else
+			std::cout << 0;
+	}
+
+private:
+	void input()
+	{
+		int e;
+		std::cin >> mN >> e;
+		mN = (mN << 1) +2;
+		mGraph.resize(mN);
+
+		int a, b;
+		while (e--)
+		{
+			std::cin >> a >> b;
+			if (a < 0)
+			{
+				a = (a * -1) << 1;
+				if (b < 0)
+				{
+					b = (b * -1) << 1;
+					mGraph[a].push_back(b + 1);
+					mGraph[b].push_back(a + 1);
+				}
+				else
+				{
+					b <<= 1;
+					mGraph[a].push_back(b);
+					mGraph[b + 1].push_back(a + 1);
+				}
+			}
+			else
+			{
+				a <<= 1;
+				if (b < 0)
+				{
+					b = (b * -1) << 1;
+					mGraph[a + 1].push_back(b + 1);
+					mGraph[b].push_back(a);
+				}
+				else
+				{
+					b <<= 1;
+					mGraph[a + 1].push_back(b);
+					mGraph[b + 1].push_back(a);
+				}
+			}
+		}
+	}
+
+	void calc()
+	{
+		mDisc.resize(mN, -1);
+		mTransSccidx.resize(mN, -1);
+		mbCheck = new bool[mN]();
+		mbSccCheck = new bool[mN]();
+
+		int cnt = 0, scccnt = 0;
+		for (int i = 2; i < mN; i++)
+		{
+			if (mbAns && !mbCheck[i])
+				tarjan(i, cnt, scccnt);
+		}
+	}
+
+	int tarjan(int now, int& cnt, int& scccnt)
+	{
+		mSt.push(now);
+		mbCheck[now] = true;
+		int par = mDisc[now] = cnt++;
+
+		for (auto& next : mGraph[now])
+		{
+			if (!mbCheck[next])
+				par = std::min(par, tarjan(next, cnt, scccnt));
+			else if (!mbSccCheck[next])
+			{
+
+				par = std::min(par, mDisc[next]);
+			}
+		}
+
+		if (par == mDisc[now])
+		{
+			while (true)
+			{
+				int top = mSt.top();
+				mSt.pop();
+
+				if (top & 1)
+				{
+					if (mTransSccidx[top - 1] == scccnt)
+						mbAns = false;
+				}
+				else
+				{
+					if (mTransSccidx[top + 1] == scccnt)
+						mbAns = false;
+				}
+
+				mbSccCheck[top] = true;
+				mTransSccidx[top] = scccnt;
+				if (now == top)
+					break;
+			}
+
+			scccnt++;
+		}
+
+
+		return par;
+	}
+
+private:
+	bool mbAns = true;
+	bool *mbCheck, *mbSccCheck;
+	int mN;
+	vi mDisc, mTransSccidx;
+	vvi mGraph;
+	std::stack<int> mSt;
+};
+
+void Solve()
+{
+	SCC scc;
+	scc.PrintAns();
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+
+	Solve();
+
+	return 0;
+}
