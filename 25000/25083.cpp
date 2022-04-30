79c24de7ca3a3dc0478e4bd36dae7e56ad8c382f
Phildy <86285421+pianoop@users.noreply.github.com>
2022-04-30 13:24:39 +0900

    baekjoon algorithm
    Implementation

---
+++ 25000/25083.cpp
@@ -0,0 +1,23 @@
+//새싹
+//-------------------------------------------------------------------
+#include <iostream>
+
+
+void Solve()
+{
+	std::cout << "         ,r'\"7\n";
+	std::cout << "r`-_   ,'  ,/\n";
+	std::cout << " \\. \". L_r'\n";
+	std::cout << "   `~\\/\n";
+	std::cout << "      |\n";
+	std::cout << "      |\n";
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+
+	Solve();
+
+	return 0;
+}
