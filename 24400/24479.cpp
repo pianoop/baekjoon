20dfe1d2e2f04d582502b9a91883e0ae11fc55b5
Phildy <86285421+pianoop@users.noreply.github.com>
2022-05-22 14:03:48 +0900

    baekjoon algorithm
    DFS

---
+++ 24400/24479.cpp
@@ -0,0 +1,77 @@
+//알고리즘 수업 - 깊이 우선 탐색 1
+//-------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+#include <algorithm>
+typedef std::vector<int> vi;
+typedef std::vector<vi> vvi;
+
+class Q
+{
+public:
+	Q()
+	{
+		input();
+	}
+
+	void Solve()
+	{
+		int cnt = 1;
+		recur(mS, cnt);
+
+		for (int i = 1; i < mN; i++)
+			std::cout << mCheck[i] << "\n";
+	}
+
+private:
+	void input()
+	{
+		int e;
+		std::cin >> mN >> e >> mS;
+
+		mN++;
+		mEdges.resize(mN);
+		mCheck.resize(mN);
+
+		int a, b;
+		while (e--)
+		{
+			std::cin >> a >> b;
+			mEdges[a].push_back(b);
+			mEdges[b].push_back(a);
+		}
+	}
+
+	void recur(int now, int& cnt)
+	{
+		mCheck[now] = cnt;
+
+		std::sort(mEdges[now].begin(), mEdges[now].end());
+		for (auto& next : mEdges[now])
+		{
+			if (mCheck[next] == 0)
+				recur(next, ++cnt);
+		}
+	}
+
+private:
+	vvi mEdges;
+	vi mCheck;
+	int mN, mS;
+
+};
+
+void Solve()
+{
+	Q q;
+	q.Solve();
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+
+	Solve();
+
+	return 0;
+}
