6dadc291d6b2a1b82f2b87f9ffb6408e35c296b7
Phildy <86285421+pianoop@users.noreply.github.com>
2022-05-31 07:40:42 +0900

    baekjoon algorithm

---
+++ 02500/2558.cpp
@@ -0,0 +1,21 @@
+//A+B - 2
+//-------------------------------------------------------------------
+#include <iostream>
+
+
+void Solve()
+{
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << a + b;
+
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+
+	Solve();
+
+	return 0;
+}
