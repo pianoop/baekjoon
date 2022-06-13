71d32e86b8eeb4243968379102baa364e481e554
Phildy <86285421+pianoop@users.noreply.github.com>
2022-06-13 22:43:04 +0900

    baekjoon algorithm

---
+++ 02700/2743.cpp
@@ -0,0 +1,12 @@
+//단어 길이 재기
+//-------------------------------------------------------------------
+#include <iostream>
+
+int main()
+{
+    std::string str;
+    std::cin>>str;
+    std::cout<<str.length();
+    
+    return 0;
+}
