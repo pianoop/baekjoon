b1a5f10d4db2703649f52156eb6aaae3d18665e3
Phildy <86285421+pianoop@users.noreply.github.com>
2022-06-14 21:18:13 +0900

    Create 3003.cpp

---
+++ 03000/3003.cpp
@@ -0,0 +1,13 @@
+//킹, 퀸, 룩, 비숍, 나이트, 폰
+//---------------
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d, e, f;
+    std::cin>> a >> b >> c >> d >> e >> f;
+    std::cout<< 1 - a << " " << 1 - b << " " << 2 - c << " " << 2 - d << " " << 2 - e << 
+        " " << 8 - f;
+    
+    return 0;
+}
