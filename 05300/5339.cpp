e455305ca6c014a9095771e26dd4d78daa89201c
Phildy <86285421+pianoop@users.noreply.github.com>
2022-06-18 15:39:53 +0900

    baekjoon algorithm

---
+++ 05300/5339.cpp
@@ -0,0 +1,19 @@
+//콜센터
+//-------------------------------------------------------------------
+#include <iostream>
+
+int main()
+{
+    std::cout<<"     /~\\\n";
+    std::cout<<"    ( oo|\n";
+    std::cout<<"    _\\=/_\n";
+    std::cout<<"   /  _  \\\n";
+    std::cout<<"  //|/.\\|\\\\\n";
+    std::cout<<" ||  \\ /  ||\n";
+    std::cout<<"============\n";
+    std::cout<<"|          |\n";
+    std::cout<<"|          |\n";
+    std::cout<<"|          |\n";
+    
+    return 0;
+}
